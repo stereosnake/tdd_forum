da49716e6c2e63c1e351c44dfcadae9cd19988e6
Illia <illia@loveknitting.com>
2018-01-12 16:47:30 +0200

    How to test validation

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -28,4 +28,16 @@ class ParticipateInForumTest extends TestCase
             ->post('threads/some-channel/1/replies', [])
             ->assertRedirect('/login');
    }
+
+   /**
+    * @test
+    */
+    public function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+    }
 }
