2483646cb0da8e8808274265c8f50796083bdc52
Illia <illia@loveknitting.com>
2018-01-24 19:15:46 +0200

    Removing lots of redundant queries

--- app/Thread.php
+++ app/Thread.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $guarded = [];
+
+    protected $with = ['creator', 'channel'];
+
     const CREATE_PATH = '/threads/create';
 
     protected static function boot()
